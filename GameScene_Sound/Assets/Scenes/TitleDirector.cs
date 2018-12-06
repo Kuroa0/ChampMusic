0f19bb79bd96e48c608a9005f0bdfe009f016731
SomeTake <tp1002217g@gmail.com>
2018-12-06 18:57:49 +0900

    ver1.0 masterup

---
+++ GameScene_Sound/Assets/Scenes/TitleDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // シーン遷移に必要
+
+public class TitleDirector : MonoBehaviour
+{
+    public bool _TitleFlag = false;
+    void Start()
+    {
+        Invoke("TouchOK",1);
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_TitleFlag)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+
+    void TouchOK()
+    {
+        _TitleFlag = true;
+    }
+}
